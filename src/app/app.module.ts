912b96893c998d13dfdfb1bd070fc3989d4c958a
MarceloKeijiDoi <marcelokeijidoi1981@gmail.com>
2020-09-07 20:54:59 -0300

    Entrega Completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {FatecIpiCursosComponent} from './fatec-ipi-cursos/fatec-ipi-cursos.component';
+import { FatecIpiCursosCliComponent } from './fatec-ipi-cursos-cli/fatec-ipi-cursos-cli.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FatecIpiCursosComponent,
+    FatecIpiCursosCliComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fatec-ipi-cursos-cli/fatec-ipi-cursos-cli.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-fatec-ipi-cursos-cli',
+  templateUrl: './fatec-ipi-cursos-cli.component.html',
+  styleUrls: ['./fatec-ipi-cursos-cli.component.css']
+})
+export class FatecIpiCursosCliComponent {}
