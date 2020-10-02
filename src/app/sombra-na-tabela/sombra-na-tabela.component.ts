6691e024adb07cc0207a500bc6b24d6dd03b3cd1
MarceloKeijiDoi <marcelokeijidoi1981@gmail.com>
2020-10-01 21:11:57 -0300

    Versao Final

--- src/app/sombra-na-tabela/sombra-na-tabela.component.ts
+++ src/app/sombra-na-tabela/sombra-na-tabela.component.ts
@@ -8,10 +8,6 @@ private elementRef: ElementRef,
 private renderer: Renderer2
 ) {
 
- /* this.renderer.setStyle(
-    this.elementRef.nativeElement,
-    'box-shadow', '10px 10px '
-    );*/
 
  }
  @HostListener('mouseover')quandoOMousePassarPorCima(){
